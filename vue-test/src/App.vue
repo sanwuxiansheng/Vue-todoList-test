e68ebb4d74b43666056ed214229fd5840161806d
chenggongcai <1197063758@qq.com>
2019-07-07 12:09:02 +0800

    新建vue-todoList-test仓库，并完成页面的静态资源显示

---
+++ vue-test/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <todoHeard/>
+      <todoList :todos="todos"/>
+      <todoFooder/>
+    </div>
+  </div>
+</template>
+<script>
+// 引入子组件
+import todoHeard from './components/todoHeard';
+import todoList from './components/todoLIst';
+import todoFooder from './components/todoFooder'
+export default {
+  name: 'App',
+  components:{
+    todoHeard,
+    todoList,
+    todoFooder
+  },
+  data () {
+    return {
+      todos:[
+        {id:1, title:'宝马', isShow:false},
+        {id:2, title:'奥迪', isShow:false},
+        {id:3, title:'奔驰', isShow:false},
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
---
+++ vue-test/src/components/todoLIst.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul class="todo-main">
+        <todoItem v-for="todo in todos" :key="todo.id" :todo="todo"/>
+    </ul>
+</template>
+<script>
+import todoItem from './todoItem';
+export default {
+    // 父子之间通信通过props
+    props:['todos'],
+    components:{
+        todoItem
+    }
+}
+</script>
+<style scoped>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
