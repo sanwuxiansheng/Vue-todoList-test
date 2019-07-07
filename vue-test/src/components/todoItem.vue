6f9b0644d8bb2bf7d1f771853388cfea77596f2d
chenggongcai <1197063758@qq.com>
2019-07-07 13:13:53 +0800

    完成鼠标移入移出事件

--- vue-test/src/components/todoItem.vue
+++ vue-test/src/components/todoItem.vue
@@ -1,16 +1,41 @@
 <template>
-<li>
+<li :style="{backgroundColor:bgColor,color:fontClor}"
+    @mouseenter="mouseMove(true)"
+    @mouseleave="mouseMove(false)"
+>
     <label>
         <input type="checkbox" v-model="todo.isShow"/>
         <span>{{todo.title}}</span>
     </label>
-    <button class="btn btn-danger" style="display:none">删除</button>
+    <button class="btn btn-danger" style="display:none" v-show="isDisplay">删除</button>
 </li>
 </template>
 <script>
 export default {
     props:{
         todo:Object
+    },
+    data () {
+        return {
+            bgColor:'white',
+            fontClor:'black',
+            isDisplay:false
+        }
+    },
+    methods:{
+        mouseMove (flage) {
+            // 如果传入的参数为true则说明是鼠标移入事件
+            // 如果传入的参数是false则说明是鼠标移出事件
+            if (flage) {
+                this.bgColor='#ddd',
+                this.fontClor='blue',
+                this.isDisplay=true
+            } else {
+                this.bgColor='white',
+                this.fontClor='black',
+                this.isDisplay=false
+            }
+        }
     }
 }
 </script>
