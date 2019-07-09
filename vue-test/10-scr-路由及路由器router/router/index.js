397358d507fc30f52540b03dab518905a757216d
chenggongcai <1197063758@qq.com>
2019-07-09 22:24:26 +0800

    完成路由器及路由组件的使用(router))

---
+++ vue-test/10-scr-路由及路由器router/router/index.js
@@ -0,0 +1,13 @@
+// 引入Vue
+import Vue from 'vue';
+// 引入路由对象
+import VueRouter from 'vue-router';
+// 引入路由
+import routes from './routes';
+// 声明使用插件
+Vue.use(VueRouter)
+// 挂载到vue实例对象上
+export default new VueRouter({
+    mode:'history',
+    routes
+})
